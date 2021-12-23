f5a8ea6db86e95cbaa7c18d941692cfc44bd45d2
kvd <papioni229@gmail.com>
2021-12-23 21:44:32 +0300

    Update rebase.sh
    oh no

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> parent of 9724760 (git-rebase 2)
     count=$(( $count + 1 ))
 done
 
